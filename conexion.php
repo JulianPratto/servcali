cda6a308199bec9c269b5828eadafb012aea35ef
JulianPrato <julianchoprato31@gmail.com>
2021-11-30 19:28:17 -0500

    laravel

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "ciudadanosano";
-$conexion = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-?>
--- php_operations/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server = 'localhost:3306';
-$username = 'root';
-$password = '';
-$database = 'ciudadanosano';
-  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-?>
